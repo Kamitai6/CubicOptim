52051fd8a8f878f219090380e661973a62fa6e93
Kamitai <45064717+Kamitai6@users.noreply.github.com>
2022-09-01 00:37:35 +0900

    Add files via upload

--- optim.py
+++ optim.py
@@ -17,15 +17,39 @@ import csv
 '''
 parameters
 '''
+# 走った
 start = [0.0, 0.0, 0.0]  # x y Θ
-point = [[0.2, 3.2, 0.0], [1.1, 3.15, 90.0], [1.25, 0.0, 0.0], [
-    2.97, 0.33, 0.0], [2.25, 1.35, 0.0], [2.97, 2.4, 0.0], [3.1, 3.6, 0.0]]
-estimate_number = [1, 1, 1, 1, 2, 2, 1]
+point = [[0.0, 3.2, 0.0], [1.1, 3.25, 0.0], [1.25, 0.0, 0.0],
+    [3.0, 0.3, 0.0], [3.0, 1.0, 0.0], 
+    [2.2, 1.3, 0.0], [2.2, 2.0, 0.0],  
+    [3.05, 2.3, 0.0], [3.1, 3.6, 0.0]]
+estimate_number = [0, 1, 1, 1, 1, 1, 1, 1, 1]
 
-vel_acc = [[0.0, 5.0], [2.0, 2.0], [-1.0, 5.0], [1.0, 3.0],
-           [1.0, 3.0], [1.0, 3.0], [0.0, 3.0]]  # -1 --> stop
-v_max = 3.0
-frequency = 1000  # hz
+# 走った
+vel_acc = [[-1.0, 2.0], [1.0, 1.0], [-1.0, 2.0], 
+    [1.0, 2.0], [1.0, 2.0], 
+    [1.0, 2.0], [1.0, 2.0], 
+    [1.0, 2.0], [0.0, 2.0]]  # -1 --> stop
+v_max = 2.0
+
+# 戻る
+# start = [3.1, 3.6, 0.0]  # x y Θ
+# point = [[3.2, 0.0, 0.0], [1.25, 0.0, 0.0], [1.25, 3.25, 0.0],
+#     [0.0, 3.25, 0.0], [0.0, 0.0, 0.0]]
+
+# estimate_number = [0, 0, 0, 0, 0]
+
+# vel_acc = [[-1.0, 1.0], [-1.0, 1.0], [-1.0, 1.0], [-1.0, 1.0], [-1.0, 1.0]]  # -1 --> stop
+# v_max = 1.0
+
+# やばかった
+# vel_acc = [[-1.0, 3.0], [2.0, 2.0], [-1.0, 3.0], 
+#     [2.0, 3.0], [2.0, 3.0], 
+#     [2.0, 3.0], [2.0, 3.0], 
+#     [2.0, 3.0], [0.0, 3.0]]  # -1 --> stop
+# v_max = 3.0
+
+frequency = 500  # hz
 
 field = [[-0.55, -0.55], [3.55, 3.95]]  # lower_left to upper_right
 wall_thick = 0.038
@@ -39,13 +63,13 @@ poll1 = [[2.61-(0.118/2), 0.65-(0.118/2)], [0.118, 0.118]]
 poll2 = [[2.61-(0.118/2), 1.65-(0.118/2)], [0.118, 0.118]]
 poll3 = [[2.61-(0.118/2), 2.65-(0.118/2)], [0.118, 0.118]]
 
-robot_size = 0.5  # m
+robot_size = 0.6  # m
 roll_length = 0.3  # m
 
 l_gain = 1
-c_gain = 1
+c_gain = 3
 
-show_map = False
+show_map = True
 
 param = []
 for i in range(len(point)):
@@ -70,26 +94,35 @@ functions
 
 
 def spline(x, y):
-    num = int(frequency/10)
-    sp = Spline2D(x, y)
-    l_x, l_y, l_k, length = [], [], [], []
-    length_list = np.array([])
-    for i in range(len(x)-1):
-        r_x, r_y = [], []
-        x_tmp, y_tmp = [], []
-        s = np.linspace(sp.s[i], sp.s[i+1], num+1)[:-1]
-        for i_s in s:
-            ix, iy = sp.calc_position(i_s)
-            x_tmp.append(ix)
-            y_tmp.append(iy)
-            l_k.append(sp.calc_curvature(i_s))
-        r_x.extend(x_tmp)
-        r_y.extend(y_tmp)
-        l_x.extend(x_tmp)
-        l_y.extend(y_tmp)
-        l = np.append(np.array([0]), np.hypot(np.diff(r_x), np.diff(r_y)))
-        length_list = np.append(length_list, l)
-        length.append(np.sum(l))
+    num = 100
+    if len(x) < 2:
+        print("you are stupid")
+    if len(x) == 2:
+        length = [np.hypot(x[0]-x[1], y[0]-y[1])]
+        length_list = np.full(int(length[0]*num), length[0] / int(length[0]*num))
+        l_x = np.linspace(x[0], x[1], int(length[0]*num+1))[:-1].tolist()
+        l_y = np.linspace(y[0], y[1], int(length[0]*num+1))[:-1].tolist()
+        l_k = np.zeros(int(length[0]*num)).tolist()
+    else:
+        sp = Spline2D(x, y)
+        l_x, l_y, l_k, length = [], [], [], []
+        length_list = np.array([])
+        for i in range(len(x)-1):
+            r_x, r_y = [], []
+            x_tmp, y_tmp = [], []
+            s = np.linspace(sp.s[i], sp.s[i+1], num+1)[:-1]
+            for i_s in s:
+                ix, iy = sp.calc_position(i_s)
+                x_tmp.append(ix)
+                y_tmp.append(iy)
+                l_k.append(sp.calc_curvature(i_s))
+            r_x.extend(x_tmp)
+            r_y.extend(y_tmp)
+            l_x.extend(x_tmp)
+            l_y.extend(y_tmp)
+            l = np.append(np.array([0]), np.hypot(np.diff(r_x), np.diff(r_y)))
+            length_list = np.append(length_list, l)
+            length.append(np.sum(l))
 
     return l_x, l_y, l_k, length, length_list
 
@@ -98,6 +131,7 @@ def calcTrajectory(param):
     nega = [0] + [i for i in range(len(vel_acc))
                   if vel_acc[i][0] < 0] + [len(vel_acc)-1]
     r_x, r_y, r_k, length, length_list = [], [], [], [], []
+    index_size_list = []
     p_x, p_y = [], []
     e = 0
     for i in range(len(nega)-1):
@@ -113,10 +147,12 @@ def calcTrajectory(param):
 
         for j in range(nega[i]+int(i > 0), nega[i+1]+1):
             x_tmp, y_tmp = [], []
+            if estimate_number[j] == 0:
+                e -= 1
             for k in range(estimate_number[j]):
                 e += k
-                x_tmp.append(param[2*j+2*e])
-                y_tmp.append(param[2*j+2*e+1])
+                x_tmp.append(param[2*j + 2*e])
+                y_tmp.append(param[2*j + 2*e + 1])
             p_x.extend(x_tmp + [point[j][0]])
             p_y.extend(y_tmp + [point[j][1]])
             px.extend(x_tmp + [point[j][0]])
@@ -125,6 +161,7 @@ def calcTrajectory(param):
         x, y, k, l, list = spline(px, py)
         r_x.extend(x), r_y.extend(y), r_k.extend(
             k), length.extend(l), length_list.extend(list)
+        index_size_list.append(len(x))
 
     if show_map:
         ax.cla()
@@ -182,7 +219,7 @@ def calcTrajectory(param):
         ax.scatter(p_x, p_y, s=10, label="traj", c="orange")
         plt.pause(0.001)
 
-    return p_x, p_y, r_x, r_y, r_k, length, length_list
+    return p_x, p_y, r_x, r_y, r_k, length, length_list, index_size_list
 
 
 def judgeCross(a, b, c, d):
@@ -249,7 +286,14 @@ def cross_cons(param):
     return num
 
 # 右行け左行け
+# TODO
 
+
+def checkKeta(c):
+    if c == '0.0':
+        return '0.000'
+    else:
+        return c
 
 def main():
     # 制約条件式が0以上になるようにする
@@ -263,7 +307,7 @@ def main():
     result = minimize(func, x0=param, constraints=cons, method="COBYLA")
     print(result)
 
-    p_x, p_y, r_x, r_y, _, length, length_list = calcTrajectory(result.x)
+    p_x, p_y, r_x, r_y, _, length, length_list, index_size_list = calcTrajectory(result.x)
 
     '''
     plot parameters
@@ -282,20 +326,22 @@ def main():
     x_start, x_target = 0.0, 0.0
     v_start, v_target = 0.0, 0.0
     t_list = []
+    start_num = [0] + [i for i in range(len(vel_acc))
+                  if vel_acc[i][0] < 0]
     e = 0
 
     for i in range(len(vel_acc)):
         x_target += length[i*2+e]
-        for j in range(1, estimate_number[i]+1):
-            x_target += length[i*2+e+j]
+        for j in range(estimate_number[i]):
+            x_target += length[i*2+e+j+1]
 
         vel_acc[i][0] = 0 if vel_acc[i][0] < 0 else vel_acc[i][0]
         v_target = vel_acc[i][0]
         (Y, Yd, _, t) = profile.plan(x_start, x_target, v_start,
                                      v_target, v_max, vel_acc[i][1], vel_acc[i][1], frequency)
         x_start += length[i*2+e]
-        for j in range(1, estimate_number[i]+1):
-            x_start += length[i*2+e+j]
+        for j in range(estimate_number[i]):
+            x_start += length[i*2+e+j+1]
 
         v_start = vel_acc[i][0]
         t_list.append(t[-1])
@@ -315,35 +361,57 @@ def main():
     '''
     adjustment velocity
     '''
-    plot_t = [0.0]
+    plot_t = []
     x_start, x_target = 0.0, 0.0
     v_start, v_target = 0.0, 0.0
     t_list = []
-    stack_t = 0
-    stack_y = np.array([0.0])
     e = 0
+    p = 0
+    # for save data
+    stack_t = 0
+    stack_y = []
+    stack_len = []
 
     for i in range(len(vel_acc)):
         x_target += length[i*2+e]
-        for j in range(1, estimate_number[i]+1):
-            x_target += length[i*2+e+j]
+        for j in range(estimate_number[i]):
+            x_target += length[i*2+e+j+1]
         v_target = vel_acc[i][0] - omega[i] * vel_acc[i][0] / v_max
         (Y, Yd, _, t) = profile.plan(x_start, x_target, v_start,
-                                     v_target, v_max-omega[i], vel_acc[i][1], vel_acc[i][1], frequency)
+                                    v_target, v_max-omega[i], vel_acc[i][1], vel_acc[i][1], frequency)
         x_start += length[i*2+e]
-
         x_start += sum([
-            length[i*2+e+j] for j in range(1, estimate_number[i]+1)
+            length[i*2+e+j+1] for j in range(estimate_number[i])
         ])
         v_start = vel_acc[i][0] - omega[i] * vel_acc[i][0] / v_max
         t_list.append(t[-1])
-        e += estimate_number[i]-1
-
+        
         ax1.plot(stack_t+t, Y, label=str(i))  # Pos
         ax2.plot(stack_t+t, Yd, label=str(i))  # Vel
-        plot_t.extend(stack_t+t[1:])
+        
+        if np.shape(stack_y)[0] != p+1:
+            stack_y.append(Y[:])
+            stack_len.append(length[i*2+e])
+            stack_len[p] += sum([
+                length[i*2+e+j+1] for j in range(estimate_number[i])
+            ])
+            stack_t = 0
+            plot_t.append([0.0])
+            plot_t[p].extend(stack_t+t[1:])
+        else:
+            stack_y[p].extend(Y[1:])
+            stack_len[p] += length[i*2+e]
+            stack_len[p] += sum([
+                length[i*2+e+j+1] for j in range(estimate_number[i])
+            ])
+            plot_t[p].extend(stack_t+t[1:])
+        if len(start_num) > p+1:
+            if start_num[p+1] == i: p+=1
+
+        e += estimate_number[i]-1
         stack_t += t[-1]
-        stack_y = np.append(stack_y, np.array(Y[1:]))
+
+    stack_y = np.array(stack_y)
 
     '''
     adjustment angular velocity
@@ -351,8 +419,9 @@ def main():
     theta = [start[2]] + [point[i][2] for i in range(len(point))]
     omega = []
     th_start, th_target = 0.0, 0.0
+    p = 0
     stack_t = 0
-    stack_th = np.array([0.0])
+    stack_th = []
 
     for i in range(len(theta)-1):
         if theta[i] != theta[i+1]:
@@ -368,60 +437,103 @@ def main():
             ax5.plot(stack_t+t, THd, label=str(i))  # Vel
             th_start += abs(theta[i+1] - theta[i])
             stack_t += t[-1]
-            stack_th = np.append(stack_th, np.array(TH[1:]))
+            if np.shape(stack_th)[0] != p+1:
+                stack_th.append(TH[:])
+            else:
+                stack_th[p].extend(TH[1:])
+            if len(start_num) > p+1:
+                if start_num[p+1] == i: p+=1
         else:
             omega.append(0.0)
-            stack_th = np.append(stack_th, np.zeros(int(t_list[i]*frequency)))
+            if np.shape(stack_th)[0] != p+1:
+                stack_th.append([0.0] * (int(t_list[i]*frequency)+1))
+            else:
+                stack_th[p].extend([0.0] * int(t_list[i]*frequency))
+            if len(start_num) > p+1:
+                if start_num[p+1] == i: p+=1
+    
+    stack_th = np.array(stack_th)
 
     '''
     fusion
     '''
     trajectory = []
-    pre_index = 0
-    count = 0
     length_cum = np.cumsum(length_list)
-    
-    for y, th in zip(stack_y, stack_th):
-        idx = np.abs(np.asarray(length_cum) - y).argmin()
-        if idx != 0 and pre_index != idx or idx == len(r_x)-1:
-            if count > 1:
-                dx = (r_x[idx]-r_x[pre_index])/count
-                dy = (r_y[idx]-r_y[pre_index])/count
-                for i in range(count):
-                    trajectory.append((r_x[pre_index]+dx*i, r_y[pre_index]+dy*i, th))
-                count = 1
-                if idx == len(r_x)-1:
+    index_size_cum = np.cumsum(index_size_list)
+
+    last_index = 0
+    for j in range(len(stack_y)):
+        out_trajectory = []
+        count = 1
+        pre_index = 0
+        for y, th in zip(stack_y[j], stack_th[j]):
+            idx = np.abs(np.asarray(length_cum) - y).argmin()
+            if (idx != last_index and pre_index != idx) or idx == index_size_cum[j]-1:
+                if count > 1:
+                    dx = (r_x[idx]-r_x[pre_index])/count
+                    dy = (r_y[idx]-r_y[pre_index])/count
+                    for i in range(count):
+                        trajectory.append((r_x[pre_index]+dx*i, r_y[pre_index]+dy*i, th))
+                        out_trajectory.append((r_x[pre_index]+dx*i, r_y[pre_index]+dy*i, th))
+                    count = 1
+                    if idx == index_size_cum[j]-1:
+                        trajectory.append((r_x[idx], r_y[idx], th))
+                        out_trajectory.append((r_x[idx], r_y[idx], th))
+                else:
                     trajectory.append((r_x[idx], r_y[idx], th))
+                    out_trajectory.append((r_x[idx], r_y[idx], th))
             else:
-                trajectory.append((r_x[idx], r_y[idx], th))
-        else:
-            count += 1
-        pre_index = idx
+                count += 1
+            pre_index = idx
+        last_index = pre_index
+
+        print(len(stack_y[j]))
+        print(len(plot_t[j]))
+        print(len(out_trajectory))
+
+        t = checkKeta(str(round(plot_t[j][0], 3)))
+        x = checkKeta(str(round(out_trajectory[0][0]*1000, 3)))
+        y = checkKeta(str(round(out_trajectory[0][1]*1000, 3)))
+        th = checkKeta(str(round(out_trajectory[0][2]*1000, 3)))
+        vx = checkKeta(str(round(0.0, 3)))
+        vy = checkKeta(str(round(0.0, 3)))
+        v = checkKeta(str(round(0.001, 3)))
+        argv = str(math.atan2(out_trajectory[1][1]-out_trajectory[0][1], out_trajectory[1][0]-out_trajectory[0][0]))
+        omega = checkKeta(str(round(0.0, 3)))
+
+        push_data = [[t, x, y, th, vx, vy, v, argv, omega]]
+        for i in range(1, len(plot_t[j])):
+            t = checkKeta(str(round(plot_t[j][i], 3)))
+            x = checkKeta(str(round(out_trajectory[i][0]*1000, 3)))
+            y = checkKeta(str(round(out_trajectory[i][1]*1000, 3)))
+            th = checkKeta(str(round(out_trajectory[i][2]*1000, 3)))
+            vx = checkKeta(str(round((out_trajectory[i][0]-out_trajectory[i-1][0])*frequency*1000, 3)))
+            vy = checkKeta(str(round((out_trajectory[i][1]-out_trajectory[i-1][1])*frequency*1000, 3)))
+            v = checkKeta(str(round(np.hypot(out_trajectory[i][0]-out_trajectory[i-1][0], out_trajectory[i][1]-out_trajectory[i-1][1])*frequency*1000, 3)))
+            argv = checkKeta(str(math.atan2(out_trajectory[i][1]-out_trajectory[i-1][1], out_trajectory[i][0]-out_trajectory[i-1][0])))
+            omega = checkKeta(str(round((out_trajectory[i][2]-out_trajectory[i-1][2])*frequency*1000, 3)))
+            push_data.append([t, x, y, th, str(vx), str(vy), str(v), str(argv), str(omega)])
+
+        with open('test'+str(j)+'.csv', 'w', newline="") as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(['LABEL t', 'x', 'y', 'theta', 'vx', 'vy', '|v|', 'arg(v)', 'omega'])
+            writer.writerow(['INTERVALTIME 0.001'])
+            writer.writerow(['START 0.000', 
+                            checkKeta(str(round(out_trajectory[0][0]*1000, 3))), 
+                            checkKeta(str(round(out_trajectory[0][1]*1000, 3))), 
+                            checkKeta(str(round(out_trajectory[0][2]*1000, 3))), 
+                            '0.000', '0.000', '0.001', checkKeta(str(math.atan2(out_trajectory[3][1]-out_trajectory[0][1], out_trajectory[3][0]-out_trajectory[0][0]))), '0.000'])
+            writer.writerow(['END '+checkKeta(str(round(plot_t[j][-1], 3))), 
+                            checkKeta(str(round(out_trajectory[-1][0]*1000, 3))), 
+                            checkKeta(str(round(out_trajectory[-1][1]*1000, 3))), 
+                            '0.000', '0.000', '0.000', '0.001', '0.000', '0.000'])
+            writer.writerow(['LENGTH '+str(stack_len[j]*1000)])
+            writer.writerow(['TIME '+str(plot_t[j][-1])])
+            writer.writerow([])
+            writer.writerow(['MAIN'])
+            writer.writerows(push_data)
 
     ax6.plot(trajectory)
-    push_data = [['0.000', '0.000', '0.000', '0.000', '0.000', '0.000', '0.001', '0.000', '0.000']]
-    for i in range(1, len(plot_t)):
-        t = str(round(plot_t[i], 3))
-        x = str(round(trajectory[i][0]*1000, 3))
-        y = str(round(trajectory[i][1]*1000, 3))
-        th = str(round(trajectory[i][2]*1000, 3))
-        vx = round((trajectory[i][0]-trajectory[i-1][0])*frequency*1000, 3)
-        vy = round((trajectory[i][1]-trajectory[i-1][1])*frequency*1000, 3)
-        v = round(np.hypot(vx, vy), 3)
-        push_data.append([t, x, y, th, str(vx), str(vy), str(v), '0.0', '0.0'])
-
-    with open('test.csv', 'w', newline="") as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(['LABEL t', 'x', 'y', 'theta', 'vx', 'vy', '|v|', 'arg(v)', 'omega'])
-        writer.writerow(['INTERVALTIME 0.001'])
-        writer.writerow(['START 0.000', '0.000', '0.000', '0.000', '0.000', '0.000', '0.001', '0.000', '0.000'])
-        writer.writerow(['END '+str(sum(t_list)), str(trajectory[-1][0]*1000), str(trajectory[-1][1]*1000), '0.000', '0.000', '0.000', '0.001', '0.000', '0.000'])
-        writer.writerow(['LENGTH '+str(sum(length)*1000)])
-        writer.writerow(['TIME '+str(sum(t_list))])
-        writer.writerow([])
-        writer.writerow(['MAIN'])
-        writer.writerows(push_data)
-
 
     '''
     plot
